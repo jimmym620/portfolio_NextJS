bfdba6abc1459c111bf281280ef49420ab87d825
jimmym620 <jimmym620@gmail.com>
2022-04-04 22:59:35 +0100

    contact page content

--- portfolio_nextjs/pages/contact.js
+++ portfolio_nextjs/pages/contact.js
@@ -1,7 +1,23 @@
+import { SocialIcon } from "react-social-icons";
+
 const Contact = () => {
     return (
-        <div>
-            <h1>this is the contact page</h1>
+        <div className="content-container">
+            <div className="content">
+                <h1>Connect with me</h1>
+                <div className="icon-container">
+                    <SocialIcon
+                        url="https://github.com/jimmym620"
+                        target="_blank"
+                    />
+                    <SocialIcon
+                        url="https://www.linkedin.com/in/chun-ming-jimmy-man"
+                        target="_blank"
+                    />
+                </div>
+                <h3>Email: jimmym620@gmail.com</h3>
+                <h3>Phone: (+44)7587789376</h3>
+            </div>
         </div>
     );
 };
