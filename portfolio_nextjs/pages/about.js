5cd9bcbc9d5d272a9c45feb1b95215bcb8fa1568
jimmym620 <jimmym620@gmail.com>
2022-04-07 11:17:50 +0100

    menu button is hidden on desktop

--- portfolio_nextjs/pages/about.js
+++ portfolio_nextjs/pages/about.js
@@ -5,7 +5,7 @@ const About = () => {
                 <h1>A little about me</h1>
 
                 <p>
-                    I graduated from Heriot-Watt University in BSc Computer
+                    Graduated from Heriot-Watt University in BSc Computer
                     Science last year (2021).
                 </p>
                 <p>Over the last several months, I have been learning JavaScript and React, this portfolio showcases some of what I have learned so far. My goal is to become a full stack developer and I will be keeping track of my progress on this site.</p>
